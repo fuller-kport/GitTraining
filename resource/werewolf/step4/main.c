a600f68eb69d65e1a7cf671addd1dc8584a7832a
cook-a-doodle-do <densukenul@gmail.com>
2019-10-20 17:38:04 +0900

    update

--- resource/werewolf/step4/main.c
+++ resource/werewolf/step4/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 //名前の最大文字数
 #define NAME_MAX_NUM 32
@@ -20,7 +21,7 @@ typedef struct{
 	int is_live;             //生きているなら1, 死んでいるなら0
 }Player;
 
-//プレイヤー達の情報を表示
+//プレイヤーの情報を表示 今は開発しやすいように役職も表示
 void show_players(Player p[], int p_num){
 	printf("\n");
 	printf("~~~~~~~~~~~~~~~~~~~~~~~ << players! >> ~~~~~~~~~~~~~~~~~~~~~~~\n");
@@ -30,7 +31,60 @@ void show_players(Player p[], int p_num){
 	printf("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
 }
 
+//入力を全部読み捨てる
+void clean_stdin(void){
+	char c;
+	for(;;){
+		c = getchar();
+		if (c == '\n' || c == EOF) break;
+	}
+}
 
+//上の関数を待機用として用いる
+void wait_key(void) {
+	printf("<ENTER>\n");
+	clean_stdin();
+}
+
+//自分以外の生きているプレイヤーNo.を読み込む
+int read_living_other_than_myself_player_num(Player players[], int players_num, int cur_player_num){
+	int n;
+	for (;;){
+		printf("No.");
+		scanf("%d", &n);
+		if (n<0 || n>=players_num){
+			printf("player No.%d は村にいません\n", n);
+			continue;
+		}
+		if (n == cur_player_num){
+			printf("自分自身は選択出来ません\n");
+			continue;
+		}
+		if (!players[n].is_live){
+			printf("player No.%d は既に死体です\n", n);
+			continue;
+		}
+		wait_key();
+		return n;
+	}
+}
+
+//投票を集計して一人選ぶ
+int ballot_winner(int ballot_box[], int size){
+	int max = 0;//最大の得票数
+	for(int i=0; i<size; i++) {
+		if (max < ballot_box[i]) max = ballot_box[i]; 
+	}
+	int j = 0;//最大の投票数を得た人は何人いるか
+	int arr[PLAYERS_MAX_NUM];//最大の投票数の人達(j人)
+	for(int i=0; i<size; i++) {
+		if (ballot_box[max] == ballot_box[i]){
+			arr[j] = i;
+			j++;
+		}
+	}
+	return arr[rand()%j];
+}
 
 int main(void){
 	printf("人狼");
